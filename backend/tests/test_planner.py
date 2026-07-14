3306562e34237d2532710b820b53a0b53c4b8215
Dev gokha <Devgokha434@gmail.com>
2026-07-14 23:55:35 +0530

    test: remove youtube tests

--- backend/tests/test_planner.py
+++ backend/tests/test_planner.py
@@ -62,10 +62,7 @@ TEST_CASES = [
     ("draw a picture of a cat", "image"),
     ("make a picture of a landscape", "image"),
 
-    # --- YouTube Generation queries ---
-    ("summarize this youtube video https://youtube.com/watch?v=12345", "youtube"),
-    ("what is this video about", "youtube"),
-    ("explain the youtube link", "youtube"),
+
 ]
 
 
